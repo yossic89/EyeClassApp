22152f81ad27d1870ca106c60a56864208aeef21
EyeClass <34593746+EyeClass@users.noreply.github.com>
2018-04-06 18:58:24 +0300

    Initial commit

---
+++ app/src/main/java/eyeclass/eyeclassapp/ConnectionTask.java
@@ -0,0 +1,77 @@
+package eyeclass.eyeclassapp;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+
+public class ConnectionTask extends AsyncTask<String, Void, Integer> {
+    @Override
+    protected Integer doInBackground(String... params) {
+        String id = params[0];
+        String pass = params[1];
+        String data = null;
+        try {
+            data = URLEncoder.encode("id", "UTF-8")
+                    + "=" + URLEncoder.encode(id, "UTF-8");
+            data += "&" + URLEncoder.encode("password", "UTF-8")
+                    + "=" + URLEncoder.encode(pass, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        BufferedReader reader=null;
+        String userPerm = "";
+        // Send data
+        try
+        {
+            // Defined URL  where to send data
+            URL url = new URL("http://192.168.31.189:8080/EyeClass/login");
+
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.connect();
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write( data );
+            wr.flush();
+
+            // Get the server response
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                // Append server response in string
+                sb.append(line + "\n");
+            }
+
+            userPerm = sb.toString().trim();
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.toString());
+        }
+        finally
+        {
+            try
+            {
+
+                reader.close();
+            }
+
+            catch(Exception ex) {;System.out.println(ex.toString());}
+        }
+
+        // Show response on activity
+        return Integer.parseInt(userPerm);
+    }
+}
---
+++ app/src/main/java/eyeclass/eyeclassapp/MainActivity.java
@@ -0,0 +1,91 @@
+package eyeclass.eyeclassapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mIdView;
+    private EditText mPasswordView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mIdView = (EditText) findViewById(R.id.IdNum);
+
+        Button mSignInButton = (Button) findViewById(R.id.signInBtn);
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    attemptLogin();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void attemptLogin() throws UnsupportedEncodingException, ExecutionException, InterruptedException {
+
+        // Store values at the time of the login attempt.
+        String id = mIdView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError("הסיסמא צריכה להכיל יותר מ 4 תווים");
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+           // postReq(id, password);
+            int permmision = new ConnectionTask().execute(id, password).get();
+            System.out.println("YOSSISSSSSIIIII " + permmision);
+        }
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+
+
+}
+
+
