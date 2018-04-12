7390ff4451d05446cfb9e9cb82db9eacd9074102
EyeClass <34593746+EyeClass@users.noreply.github.com>
2018-04-12 11:34:53 +0300

    PDF view

---
+++ app/src/main/java/Infra/Constants.java
@@ -0,0 +1,8 @@
+package Infra;
+
+public class Constants {
+    private static String ip = "192.168.31.244";
+    public static String HOST = "http://" + ip +":8080/EyeClass";
+    public static String TeacherServlet = HOST +"/teacher";
+    public static String LoginServlet = HOST + "/login";
+}
