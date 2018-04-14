6e8ada74d29624adad7e596f528bea64a6998a05
EyeClass <34593746+EyeClass@users.noreply.github.com>
2018-04-14 09:16:03 +0300

    Student view

--- app/src/main/java/Infra/Constants.java
+++ app/src/main/java/Infra/Constants.java
@@ -1,8 +1,23 @@
 package Infra;
 
 public class Constants {
-    private static String ip = "192.168.31.244";
-    public static String HOST = "http://" + ip +":8080/EyeClass";
-    public static String TeacherServlet = HOST +"/teacher";
-    public static String LoginServlet = HOST + "/login";
+
+    public static class Connections{
+        private static String ip = "192.168.31.244";
+        public static String HOST = "http://" + ip +":8080/EyeClass";
+        public static String TeacherServlet = HOST +"/teacher";
+        public static String LoginServlet = HOST + "/login";
+        public static String StudentServlet = HOST + "/student";
+    }
+
+    public static class Teacher{
+        public static int Demo_lesson_id = 1;
+        public static String Demo_class_id = "ORT Eilat_Grade11_1";
+        public static int SendTeacherPageMS = 500;
+    }
+
+    public static class Student{
+        public static int CheckActiveLessonMS = 500;
+    }
+
 }
