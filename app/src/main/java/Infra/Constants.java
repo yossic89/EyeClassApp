c9c80dadf80ef33874550a781f08174c4b0bd292
EyeClass <34593746+EyeClass@users.noreply.github.com>
2018-04-26 14:57:02 +0300

    bug fixes for user managment hanlder

--- app/src/main/java/Infra/Constants.java
+++ app/src/main/java/Infra/Constants.java
@@ -3,17 +3,25 @@ package Infra;
 public class Constants {
 
     public static class Connections{
-        private static String ip = "192.168.31.244";
+        private static String ip = "192.168.205.144";
         public static String HOST = "http://" + ip +":8080/EyeClass";
         public static String TeacherServlet = HOST +"/teacher";
         public static String LoginServlet = HOST + "/login";
         public static String StudentServlet = HOST + "/student";
     }
 
+    public static class Permissions{
+        public static final int NoPermission = 0;
+        public static final int Admin = 1;
+        public static final int Teacher = 2;
+        public static final int Student = 3;
+    }
+
     public static class Teacher{
         public static int Demo_lesson_id = 1;
         public static String Demo_class_id = "ORT Eilat_Grade11_1";
         public static int SendTeacherPageMS = 500;
+        public static final String Class_id = "class_id";
     }
 
     public static class Student{
