12a9a9a089dbf0bd733421af7e43180120d79113
yamit <yamitm221@gmail.com>
2018-09-01 18:12:14 +0300

    add lesson, ass questions

--- app/src/main/java/eyeclass/eyeclassapp/lesson/AddQuestions.java
+++ app/src/main/java/eyeclass/eyeclassapp/lesson/AddQuestions.java
@@ -1,4 +1,43 @@
 package eyeclass.eyeclassapp.lesson;
 
-public class AddQuestions {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+import android.widget.EditText;
+
+import eyeclass.eyeclassapp.R;
+
+public class AddQuestions extends AppCompatActivity {
+    private EditText mTitle;
+    private EditText mTopic;
+    private EditText mTime;
+    private EditText mQuestion;
+    private EditText mCorrectAnswer;
+    private EditText mWrongAnswer1;
+    private EditText mWrongAnswer2;
+    private EditText mWrongAnswer3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_questions);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+        mTopic = (EditText) findViewById(R.id.topic_quest);
+        mTime = (EditText) findViewById(R.id.time_quest);
+        mQuestion = (EditText) findViewById(R.id.question_quest);
+        mCorrectAnswer = (EditText) findViewById(R.id.correct_ans_quest);
+        mWrongAnswer1 = (EditText) findViewById(R.id.wrong_ans_1_quest);
+        mWrongAnswer2 = (EditText) findViewById(R.id.wrong_ans_2_quest);
+        mWrongAnswer3 = (EditText) findViewById(R.id.wrong_ans_3_quest);
+    }
+
+    public void submitQuestion(){
+        //TODO
+    }
+
+    public void addQuestionToList(){
+        //TODO
+    }
 }
--- app/src/main/java/eyeclass/eyeclassapp/lesson/UploadLesson.java
+++ app/src/main/java/eyeclass/eyeclassapp/lesson/UploadLesson.java
@@ -1,127 +1,103 @@
 package eyeclass.eyeclassapp.lesson;
 
 import android.app.Activity;
-import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import eyeclass.eyeclassapp.R;
 
-
-public class UploadLesson extends Activity {
-
-    Button buttonOpenDialog;
-    Button buttonUp;
-    TextView textFolder;
-    static final int CUSTOM_DIALOG_ID = 0;
-    ListView dialog_ListView;
-    File root;
-    File curFolder;
+public class UploadLesson extends AppCompatActivity {
+    static final int READ_REQ = 24;
     byte[] lessonFile;
+    private EditText mClass;
+    private EditText mCurr;
+    private EditText mTitle;
 
-    private List<String> fileList = new ArrayList<String>();
+        ViewGroup cont;
+        ListView contactLst;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.opendialog);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_lesson);
 
-        buttonOpenDialog = (Button) findViewById(R.id.opendialog);
-        buttonOpenDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showDialog(CUSTOM_DIALOG_ID);
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+
+            mClass = (EditText) findViewById(R.id.class_lesson);
+            mCurr = (EditText) findViewById(R.id.curriculum_lesson);
+            mTitle = (EditText) findViewById(R.id.title_lesson);
+
+        }
+
+        public void readFile(View view) {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("application/pdf");
+            startActivityForResult(intent, READ_REQ);
+        }
+
+    public void submit(View view) {
+        //TODO
+    }
+
+    public void addQuestions(View view) {
+        Intent intent = new Intent(this, AddQuestions.class);
+        startActivity(intent);
+    }
+
+        @RequiresApi(api = Build.VERSION_CODES.M)
+        @Override
+        public void onActivityResult(int requestCode, int resultCode,
+                                     Intent resultData) {
+
+            if (resultCode == Activity.RESULT_OK) {
+
+                Uri uri = null;
+                if (resultData != null) {
+                    uri = resultData.getData();
+                }
+                if(requestCode == READ_REQ){
+                    readTextFile(uri);
+                }
             }
-        });
-
-        root = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
-        curFolder = root;
-    }
-
-    @Override
-    protected Dialog onCreateDialog(int id) {
-
-        Dialog dialog = null;
-
-        switch (id) {
-            case CUSTOM_DIALOG_ID:
-                dialog = new Dialog(UploadLesson.this);
-                dialog.setContentView(R.layout.dialoglayout);
-                dialog.setTitle("Custom Dialog");
-                dialog.setCancelable(true);
-                dialog.setCanceledOnTouchOutside(true);
-
-                textFolder = (TextView) dialog.findViewById(R.id.folder);
-                buttonUp = (Button) dialog.findViewById(R.id.up);
-                buttonUp.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        ListDir(curFolder.getParentFile());
-                    }
-                });
-
-                dialog_ListView = (ListView) dialog.findViewById(R.id.dialoglist);
-                dialog_ListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        File selected = new File(fileList.get(position));
-                        if(selected.isDirectory()) {
-                            ListDir(selected);
-                        } else {
-                            Toast.makeText(UploadLesson.this, selected.toString() + " selected",
-                                    Toast.LENGTH_LONG).show();
-                            dismissDialog(CUSTOM_DIALOG_ID);
-                            //TODO convert file to byte[]
-                            // lessonFile = selected.
-                        }
-                    }
-                });
-
-                break;
-        }
-        return dialog;
-    }
-
-    @Override
-    protected void onPrepareDialog(int id, Dialog dialog) {
-        super.onPrepareDialog(id, dialog);
-        switch (id) {
-            case CUSTOM_DIALOG_ID:
-                ListDir(curFolder);
-                break;
-        }
-    }
-
-    void ListDir(File f) {
-        if(f.equals(root)) {
-            buttonUp.setEnabled(false);
-        } else {
-            buttonUp.setEnabled(true);
         }
 
-        curFolder = f;
-        textFolder.setText(f.getPath());
+        private void readTextFile(Uri uri) {
+            InputStream inputStream = null;
+            try {
+                inputStream = getContentResolver().openInputStream(uri);
 
-        File[] files = f.listFiles();
-        fileList.clear();
+                // this dynamically extends to take the bytes you read
+                ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream();
 
-        for(File file : files) {
-            fileList.add(file.getPath());
+                // this is storage overwritten on each iteration with bytes
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+
+                // we need to know how may bytes were read to write them to the byteBuffer
+                int len = 0;
+                while ((len = inputStream.read(buffer)) != -1) {
+                    byteBuffer.write(buffer, 0, len);
+                }
+
+                //send to server as byte[]
+                lessonFile=byteBuffer.toByteArray();
+                Toast.makeText(UploadLesson.this, "Your file has been saved successfully", Toast.LENGTH_LONG).show();
+
+                inputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-
-        ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, fileList);
-        dialog_ListView.setAdapter(directoryList);
     }
-}
